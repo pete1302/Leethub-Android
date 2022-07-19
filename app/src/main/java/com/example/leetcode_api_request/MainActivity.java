10b7b80cc4f3b313b838948edbddb0d96a915945
pete1302 <63489114+pete1302@users.noreply.github.com>
2022-07-20 02:05:06 +0530

    initial commit

---
+++ app/src/main/java/com/example/leetcode_api_request/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.leetcode_api_request;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextViewResult;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextViewResult = findViewById(R.id.tvResult);
+
+        OkHttpClient client = new OkHttpClient();
+
+//        String url = "https://reqres.in/api/users?page=2";
+        String url = "https://leet-api-2.herokuapp.com/pete1302/4";
+
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    final String myResponse = response.body().string();
+
+                    MainActivity.this.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            mTextViewResult.setText(myResponse);
+                        }
+                    });
+                }
+            }
+        });
+    }
+}
