ab0a1c2a464c386871c54a4dff98a6841bb25419
pete1302 <63489114+pete1302@users.noreply.github.com>
2022-08-04 06:55:08 +0530

    workign-preodic-timer

--- app/src/main/java/com/example/leetcode_api_request/MainActivity.java
+++ app/src/main/java/com/example/leetcode_api_request/MainActivity.java
@@ -1,57 +1,73 @@
 package com.example.leetcode_api_request;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.work.Data;
+import androidx.work.PeriodicWorkRequest;
+import androidx.work.WorkInfo;
+import androidx.work.WorkManager;
 
-import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
-import okhttp3.Call;
-import okhttp3.Callback;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextViewResult;
+//    public static final String KEY_TASK_DESC = "key_task_desc";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextViewResult = findViewById(R.id.tvResult);
+//        Data data = new Data.Builder()
+//                .putString(KEY_TASK_DESC, "Hey I am sending the work data")
+//                .build();
+//
+//        Constraints constraints = new Constraints.Builder()
+//                .setRequiresCharging(true)
+//                .build();
 
-        OkHttpClient client = new OkHttpClient();
+//        final OneTimeWorkRequest request = new OneTimeWorkRequest.Builder(MyWorker.class)
+//                .build();
+        PeriodicWorkRequest request =
+                new PeriodicWorkRequest.Builder(MyWorker.class, 16, TimeUnit.MINUTES)
+                        .build();
 
-//        String url = "https://reqres.in/api/users?page=2";
-        String url = "https://leet-api-2.herokuapp.com/pete1302/4";
-
-        Request request = new Request.Builder()
-                .url(url)
-                .build();
-
-        client.newCall(request).enqueue(new Callback() {
+        findViewById(R.id.workerButton).setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onFailure(Call call, IOException e) {
-                e.printStackTrace();
-            }
-
-            @Override
-            public void onResponse(Call call, Response response) throws IOException {
-                if (response.isSuccessful()) {
-                    final String myResponse = response.body().string();
-
-                    MainActivity.this.runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            mTextViewResult.setText(myResponse);
-                        }
-                    });
-                }
+            public void onClick(View view) {
+                WorkManager.getInstance().enqueue(request);
             }
         });
+
+        final TextView textView = findViewById(R.id.tvWorker);
+
+        WorkManager.getInstance().getWorkInfoByIdLiveData(request.getId())
+                .observe(this, new Observer<WorkInfo>() {
+                    @Override
+                    public void onChanged(@Nullable WorkInfo workInfo) {
+
+                        if (workInfo != null) {
+
+                            if (workInfo.getState().isFinished()) {
+
+                                Data data = workInfo.getOutputData();
+
+
+
+//                                textView.append( + "\n");
+                            }
+
+                            String status = workInfo.getState().name();
+                            textView.append(status + "\n");
+                        }
+                    }
+                });
+
     }
 }
